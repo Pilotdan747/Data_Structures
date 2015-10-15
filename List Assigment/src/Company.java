b3ac341ba6016328e3cade9456b15fc0f0973561
Daniel Owen <Pilotdan747@gmail.com>
2015-10-15 14:27:17 -0500

    List Assignment
    Started List Assignment, need to go back and Sudo code before more dev

---
+++ List Assigment/src/Company.java
@@ -0,0 +1,12 @@
+
+public class Company {
+
+	String name;
+	String ticker;
+	DLList transactions;
+	
+	public Company(String name, String ticker) {
+		this.name = name;
+		this.ticker = ticker;
+	}
+}
